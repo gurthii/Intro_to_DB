9ce3e98570ee976233b0c955fdc6addb28a328b2
Joseph G <josef.emu@gmail.com>
2025-10-15 06:38:59 +0300

    another update

--- alx_book_store.sql
+++ alx_book_store.sql
@@ -1,7 +1,7 @@
 CREATE DATABASE IF NOT EXISTS alx_book_store;
 USE alx_book_store;
 
-CREATE Books(
+CREATE TABLE Books(
 	PRIMARY KEY book_id,
 	title VARCHAR(130),
 	FOREIGN KEY (author_id) REFERENCES Authors(author_id),
@@ -9,26 +9,26 @@ CREATE Books(
 	publication_date DATE
 )
 
-CREATE Authors(
+CREATE TABLE Authors(
 	PRIMARY KEY author_id,
 	author_name VARCHAR(215)
 )
 
 
-CREATE Customers(
+CREATE TABLE Customers(
 	PRIMARY KEY customer_id,
 	customer_name VARCHAR(215),
 	email VARCHAR(215),
 	address TEXT
 )
 
-CREATE Orders(
+CREATE TABLE Orders(
 	PRIMARY KEY order_id,
 	FOREIGN KEY (customer_id) REFERENCES Customers(customer_id),
 	order_date DATE
 )
 
-CREATE Order_Details(
+CREATE TABLE Order_Details(
 	PRIMARY KEY orderdetailid
 	FOREIGN KEY (order_id) REFERENCES Orders(order_id),
 	FOREIGN KEY (book_id) REFERENCES Books(book_id),
