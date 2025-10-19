23055715a090d7b06b94ca844e154d9bfc3d4a9d
Joseph G <josef.emu@gmail.com>
2025-10-19 23:37:32 +0300

    another update on sunday

--- task_2.sql
+++ task_2.sql
@@ -2,44 +2,44 @@
 
 USE alx_book_store;
 
--- Table: authors
-CREATE TABLE authors (
+-- Table: Authors
+CREATE TABLE Authors (
     author_id INT PRIMARY KEY,
     author_name VARCHAR(215) NOT NULL
 );
 
--- Table: books
-CREATE TABLE books (
+-- Table: Books
+CREATE TABLE Books (
     book_id INT PRIMARY KEY,
     title VARCHAR(130) NOT NULL,
     author_id INT,
     price DOUBLE NOT NULL,
     publication_date DATE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id)
+    FOREIGN KEY (author_id) REFERENCES Authors(author_id)
 );
 
--- Table: customers
-CREATE TABLE customers (
+-- Table: Customers
+CREATE TABLE Customers (
     customer_id INT PRIMARY KEY,
     customer_name VARCHAR(215) NOT NULL,
     email VARCHAR(215) NOT NULL UNIQUE,
     address TEXT
 );
 
--- Table: orders
-CREATE TABLE orders (
+-- Table: Orders
+CREATE TABLE Orders (
     order_id INT PRIMARY KEY,
     customer_id INT,
     order_date DATE NOT NULL,
-    FOREIGN KEY (customer_id) REFERENCES customers(customer_id)
+    FOREIGN KEY (customer_id) REFERENCES Customers(customer_id)
 );
 
--- Table: order_details
-CREATE TABLE order_details (
+-- Table: Order_Details
+CREATE TABLE Order_Details (
     order_detail_id INT PRIMARY KEY,
     order_id INT,
     book_id INT,
     quantity DOUBLE NOT NULL,
-    FOREIGN KEY (order_id) REFERENCES orders(order_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id)
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (book_id) REFERENCES Books(book_id)
 );
